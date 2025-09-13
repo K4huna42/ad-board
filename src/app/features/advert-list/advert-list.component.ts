f26f69f5ab0558791744bc186bb7235fbb569d3c
K4huna <isevcenko617@gmail.com>
2025-09-13 14:25:13 +0500

    chore: finish project architecture

---
+++ src/app/features/advert-list/advert-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advert-list',
+  imports: [],
+  templateUrl: './advert-list.component.html',
+  styleUrl: './advert-list.component.scss'
+})
+export class AdvertListComponent {
+
+}
